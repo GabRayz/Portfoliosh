2283f67b31a4b2f7f5bfed06e52da8b754a513e9
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-05-07 15:14:28 +0200

    init

---
+++ src/main/java/fr/gabray/portfoliosh/PortfolioshApplication.java
@@ -0,0 +1,13 @@
+package fr.gabray.portfoliosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/gabray/portfoliosh/PortfolioshApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gabray.portfoliosh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
