97d4a7714c97b31591df9f95df0bb035bee9f273
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-05-08 17:35:11 +0200

    sh: session

--- src/main/java/fr/gabray/portfoliosh/controller/PortfolioshController.java
+++ src/main/java/fr/gabray/portfoliosh/controller/PortfolioshController.java
@@ -2,17 +2,21 @@ package fr.gabray.portfoliosh.controller;
 
 import fr.gabray.portfoliosh.ast.Ast;
 import fr.gabray.portfoliosh.env.Environment;
+import fr.gabray.portfoliosh.exception.CommandRuntimeException;
 import fr.gabray.portfoliosh.lexer.Lexer;
 import fr.gabray.portfoliosh.model.SendInputModel;
 import fr.gabray.portfoliosh.parser.Parser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Map;
 
 @Controller
 public class PortfolioshController {
@@ -32,20 +36,25 @@ public class PortfolioshController {
     }
 
     @MessageMapping("/send")
-    public void send(final SendInputModel model)
+    public void send(@Payload final SendInputModel model, SimpMessageHeaderAccessor headerAccessor)
     {
         logger.debug(model.getInput());
-        Parser parser = new Parser(new Lexer(model.getInput()));
         try
         {
+            Map<String, Object> sessionAttributes = headerAccessor.getSessionAttributes();
+            if (sessionAttributes == null)
+                throw new CommandRuntimeException("Failed to initialize environment");
+            Environment env = (Environment) sessionAttributes.computeIfAbsent("env", key -> Environment.defaultEnv());
+            Parser parser = new Parser(new Lexer(model.getInput()));
             Ast ast = parser.parse();
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            ast.execute(Environment.defaultEnv(), outputStream);
+            ast.execute(env, outputStream);
             simpMessagingTemplate.convertAndSend("/sock/receive", new SendInputModel(outputStream.toString()));
         }
         catch (Exception e)
         {
             logger.error("Error while executing input", e);
+            simpMessagingTemplate.convertAndSend("/sock/receive", new SendInputModel(e.getMessage()));
         }
     }
 }
