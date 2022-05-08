b24cb30c2d4dddd2462451c302d0dd64c1f8a04c
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-05-08 18:31:54 +0200

    front: auto reconnection

--- src/main/resources/static/js/portfoliosh.js
+++ src/main/resources/static/js/portfoliosh.js
@@ -1,34 +1,50 @@
 const input = document.getElementById('sh-input');
 const outputArea = document.getElementById('output');
 
-const socket = new SockJS('/websocket');
-const stompClient = Stomp.over(socket);
-stompClient.connect({}, function (frame) {
-    console.log('Connected!');
-    stompClient.subscribe('/sock/receive', function (message) {
-        let response = JSON.parse(message.body).input;
-        console.log(response);
-        if (response.length > 0) {
-            outputArea.value += '\n' + response;
-            outputArea.rows += (response.match(/\n/g) || []).length + 1;
-        }
+let stompClient = null;
+
+function connect() {
+    const socket = new SockJS('/websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected!');
+        display('Connected!');
+        stompClient.subscribe('/sock/receive', function (message) {
+            let response = JSON.parse(message.body).input;
+            console.log(response);
+            if (response.length > 0) {
+                display(response);
+            }
+        })
     })
-})
+}
+
+function display(message) {
+    outputArea.value += '\n' + message;
+    outputArea.rows += (message.match(/\n/g) || []).length + 1;
+}
 
 document.querySelector('.input-container').onsubmit = function () {
     const userInput = input.value;
-    outputArea.value += '\nportfoliosh$ ' + userInput;
+    display('portfoliosh$ ' + userInput);
     console.log(userInput);
     input.value = '';
-    outputArea.rows += 1;
 
     window.scroll(0, window.innerHeight);
 
+    if (!stompClient.connected) {
+        console.log('Reconnecting...');
+        display("Lost connection, reconnecting...");
+        connect();
+        return;
+    }
     stompClient.send("/sock/send", {}, JSON.stringify({
         'input': userInput
     }));
 
     return false;
 }
+
+connect();
 
 outputArea.value = '';
