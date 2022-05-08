f893783e930f86bcb3e40ec8960964ca4bc6bcbe
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-05-08 19:13:19 +0200

    ls: pass path in args

--- src/main/java/fr/gabray/portfoliosh/command/LsCommand.java
+++ src/main/java/fr/gabray/portfoliosh/command/LsCommand.java
@@ -4,6 +4,7 @@ import fr.gabray.portfoliosh.env.Environment;
 import fr.gabray.portfoliosh.env.FakeFile;
 import fr.gabray.portfoliosh.exception.CommandRuntimeException;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
@@ -17,20 +18,30 @@ public class LsCommand implements Command {
     }
 
     @Override
-    public int execute(final Environment env, final OutputStream outputStream, final String... args)
+    public int execute(final Environment env, final OutputStream outputStream, final String... args) throws IOException
     {
-        Collection<FakeFile> files = env.getWorkingDirectory().getFiles();
-        files.forEach(file -> {
-            String s = file.getName() + (file.getType() == FakeFile.Type.FOLDER ? "/" : "") + "\t";
-            try
-            {
-                outputStream.write(s.getBytes(StandardCharsets.UTF_8));
-            }
-            catch (IOException e)
-            {
-                throw new CommandRuntimeException(e);
-            }
-        });
+        try
+        {
+            FakeFile folder = env.getFile(args.length == 1 ? "" : args[1]);
+            if (folder.getType() != FakeFile.Type.FOLDER)
+                return error(outputStream, "Not a directory");
+            Collection<FakeFile> files = folder.getFiles();
+            files.forEach(file -> {
+                String s = file.getName() + (file.getType() == FakeFile.Type.FOLDER ? "/" : "") + "\t";
+                try
+                {
+                    outputStream.write(s.getBytes(StandardCharsets.UTF_8));
+                }
+                catch (IOException e)
+                {
+                    throw new CommandRuntimeException(e);
+                }
+            });
+        }
+        catch (FileNotFoundException e)
+        {
+            return error(outputStream, "No such file or directory");
+        }
         return 0;
     }
 }
