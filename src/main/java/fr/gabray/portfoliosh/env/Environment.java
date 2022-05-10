e8d693a5cc048ef593d58f7404a1e8184abc624e
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-05-10 23:16:35 +0200

    environment (fix): don't auto scan resources

--- src/main/java/fr/gabray/portfoliosh/env/Environment.java
+++ src/main/java/fr/gabray/portfoliosh/env/Environment.java
@@ -3,17 +3,12 @@ package fr.gabray.portfoliosh.env;
 import fr.gabray.portfoliosh.command.sql.Column;
 import fr.gabray.portfoliosh.command.sql.Database;
 import fr.gabray.portfoliosh.command.sql.Table;
-import fr.gabray.portfoliosh.exception.EnvironmentInitException;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Map;
-import java.util.Objects;
 
 public class Environment {
 
@@ -66,20 +61,8 @@ public class Environment {
     public static Environment defaultEnv()
     {
         Environment environment = new Environment();
-        URL fsUrl = environment.getClass().getClassLoader().getResource("fs");
-        if (fsUrl == null)
-            throw new IllegalStateException("Missing fs resource folder");
-        File file;
-        try
-        {
-            file = new File(fsUrl.toURI());
-        }
-        catch (URISyntaxException e)
-        {
-            throw new EnvironmentInitException(e);
-        }
 
-        addFilesToEnv(environment, file, environment.getRootFolder());
+        addFilesToEnv(environment);
 
         Table levels = new Table("levels");
         levels.addColumn(new Column("id"), true);
@@ -104,16 +87,19 @@ public class Environment {
         return environment;
     }
 
-    private static void addFilesToEnv(Environment environment, File file, FakeFile parent)
+    private static void addFilesToEnv(Environment environment)
     {
-        for (final File listFile : Objects.requireNonNull(file.listFiles()))
-        {
-            FakeFile.Type type = listFile.isDirectory()
-                                 ? FakeFile.Type.FOLDER
-                                 : FakeFile.Type.FILE;
-            FakeFile created = environment.createFile(type, parent, listFile.getName());
-            if (type == FakeFile.Type.FOLDER)
-                addFilesToEnv(environment, listFile, created);
-        }
+        environment.createFile(FakeFile.Type.FILE, environment.getRootFolder(), "welcome.md");
+
+        FakeFile projects = environment.createFile(FakeFile.Type.FOLDER, environment.getRootFolder(), "projects");
+        environment.createFile(FakeFile.Type.FILE, projects, "42sh.md");
+        environment.createFile(FakeFile.Type.FILE, projects, "chess.md");
+        environment.createFile(FakeFile.Type.FILE, projects, "ocr.md");
+        environment.createFile(FakeFile.Type.FILE, projects, "ping.md");
+        environment.createFile(FakeFile.Type.FILE, projects, "portfoliosh.md");
+        environment.createFile(FakeFile.Type.FILE, projects, "uprising.md");
+
+        FakeFile skills = environment.createFile(FakeFile.Type.FOLDER, environment.getRootFolder(), "skills");
+        environment.createFile(FakeFile.Type.FILE, skills, "skills.sh");
     }
 }
